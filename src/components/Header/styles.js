052f1f92d8d19fdb82853eb47b8c4d7f3145fa3a
Carol Moreira <caahrolmoreira@gmail.com>
2023-05-30 14:13:49 -0300

    add components

---
+++ src/components/Header/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+
+export const HeaderContainer = styled.header`
+  height: 50px;
+  width: 100%;
+  background: #EDEDED;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`
+
+export const ButtonHeader = styled.button`
+  position: absolute;
+  right: 16px;
+  color: #4088CB;
+  font-family: 'Noto Sans';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 25px;
+  background-color: transparent;
+  border: none;
+
+`
---
+++ src/components/Textfield/Textfield.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Textfield = styled.input`
+  width: 100%;
+  border-radius: 4px;
+  border: 1px solid #D5D8DE; 
+  font-size: 16px;
+  padding: 12px;
+`
